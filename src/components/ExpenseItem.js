a0212e321da15ad38483f611adf62aefd42d43ce
Aman Kumar <vrohan319@gmail.com>
2022-08-29 22:34:06 +0530

    change data from hard coded to dynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+
+    const expenseDate = new Date(2021,2,28) ;
+    const expenseTitle = 'Car Insurance' ;
+    const expenseAmount = 294.8 ;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
